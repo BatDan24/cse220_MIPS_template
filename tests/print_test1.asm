4294c66c99bffeab9340998fbe06bc648427edf4
Dan Benz <daniel.benz@stonybrook.edu>
2025-04-13 17:13:50 -0400

    template for Spring 2025 HW4

--- tests/print_test1.asm
+++ tests/print_test1.asm
@@ -1,21 +1,29 @@
 .data
-board:  .byte
-    # 5 rows x 5 columns, showing a checkmark with 1s
-    0, 0, 0, 0, 5,    # row 0 (top of checkmark)
-    0, 0, 0, 4, 0,    # row 1
-    1, 0, 3, 0, 0,    # row 2
-    0, 2, 0, 0, 0,    # row 3
-    0, 0, 0, 0, 0     # row 4
+# testing msg if node search found or did not find something
+node_found_msg: .asciiz "Node found\n"
+not_found_msg:  .asciiz "Not found\n"
+pointer_msg:  .asciiz " Is at Address: 0x"
 
-board_height: .word 5
-board_width:  .word 5
+# for testing purposes only with prebuilt tree (since dont have insert yet) (bottom-up)
+# Level 3 leaves
+node1: .word 1, 0, 0, 0, node3 # black leaf
+node4: .word 4, 0, 0, 0, node3 # black leaf
+node6: .word 6, 0, 0, 0, node8 # black leaf
+node9: .word 10, 0, 0, 0, node8 # black leaf
+
+# Level 2 internal
+node3: .word 3, node1, node4, 1, root # red internal node
+node8: .word 8, node6, node9, 1, root # red internal node
+
+# Level 1 (root)
+root: .word 5, node3, node8, 0, 0# root, MUST be black
 
 .text
 .globl main
 # Function: main
 main:
-    # Print board
-    jal printBoard
+    la $a0, root
+    jal print_tree
 
 debug_done:
     # Exit program
@@ -25,5 +33,6 @@ debug_done:
 
 .data
 .asciiz "This is Professor Benz's extra space that is being used for preserving memory contents to avoid losing data"
-.include "hw5.asm"
+# The below file contains your code
+.include "../hw4.asm"
 
